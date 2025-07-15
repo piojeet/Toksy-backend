9b8caa74f7b9519d106f65533cfa05049ecb7958
piojeet <manjhipiojeet@gmail.com>
2025-07-15 18:07:16 +0530

    first commit

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -54,8 +54,7 @@ export async function signup(req, res) {
         res.cookie("jwt", token, {
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true, //prevent XSS attacks,
-            sameSite: "None", //prevent CSRF attacks
-            secure: true,
+            sameSite: "strict", //prevent CSRF attacks
             secure: process.env.NODE_ENV === "production"
         })
 
@@ -88,8 +87,7 @@ export async function login(req, res) {
         res.cookie("jwt", token, {
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true, //prevent XSS attacks,
-            sameSite: "None", //prevent CSRF attacks
-            secure: true,
+            sameSite: "strict", //prevent CSRF attacks
             secure: process.env.NODE_ENV === "production"
         })
 
