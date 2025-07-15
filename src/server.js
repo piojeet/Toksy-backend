c465dc6a0f15a5fa46867862baf713288f5e303b
piojeet <manjhipiojeet@gmail.com>
2025-07-15 13:20:25 +0530

    first commit

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://toksy.onrender.com"],
     credentials: true // allow frontend to send cookies
 }))
 app.use(express.json());
